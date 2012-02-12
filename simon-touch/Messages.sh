b214d6245487e9b82df8bae14eaffcb7b6d89c49
Albert Astals Cid <tsdgeos@terra.es>
2012-02-12 22:38:43 +0100

    Don't use EXTRACTQML it never worked correctly

--- simon-touch/Messages.sh
+++ simon-touch/Messages.sh
@@ -4,10 +4,8 @@
 # the results are stored in a pseudo .cpp file to be picked up by xgettext.
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 
-$EXTRACTQML `find . -name "*.qml"` >> rc.cpp
-
 # if your application contains tips-of-the-day, call preparetips as well.
 # $PREPARETIPS > tips.cpp
 # call xgettext on all source files. If your sources have other filename
 # extensions besides .cc, .cpp, and .h, just add them in the find call.
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/simon-touch.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/simon-touch.pot
