98bcaf3c2798f84949d84de1f21b836866d32c9f
Peter Grasch <grasch@simon-listens.org>
2012-03-22 08:35:11 +0100

    Adding location files

---
+++ astromobile/astrologic/location.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2011-2012, simon listens <office@simon-listens.org>
+ * Copyright (c) 2011-2012, Scuola Superiore Sant´Anna <urp@sssup.it>
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution.
+ *  * Neither the name of the organizations nor the
+ *  names of its contributors may be used to endorse or promote products
+ *  derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef LOCATION_H
+#define LOCATION_H
+#include <QString>
+#include <QPoint>
+
+class Location
+{
+public:
+    Location(const QString& name, int x, int y);
+    
+    QString name() const { return m_name; }
+    int x() const  { return m_destination.x(); }
+    int y() const  { return m_destination.y(); }
+    QPoint destination() const { return m_destination; }
+    
+private:
+    QString m_name;
+    QPoint m_destination;
+};
+
+#endif // LOCATION_H
